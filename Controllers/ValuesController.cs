dd16a0b23ddb921785187332f260e6878da24711
julie <julielerman@gmail.com>
2017-10-06 12:06:31 -0400

    small change to get controller method to see how quickly this is available on azure

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace AngularMix2017.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2","value3" };
         }
 
         // GET api/values/5
