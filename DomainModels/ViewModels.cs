6838bf34ddb629a0a1709f50e905bdd1ab8eb5d2
julie <julielerman@gmail.com>
2017-11-02 11:55:17 -0700

    better view models

--- DomainModels/ViewModels.cs
+++ DomainModels/ViewModels.cs
@@ -1,19 +1,28 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json;
 
-namespace Firefly.Domain {
-  public class CharacterViewModel {
+namespace Firefly.Domain
+{
+  public class CharacterViewModel
+  {
     public string CharacterName { get; set; }
     public List<QuoteViewModel> Quotes { get; set; }
+    [JsonProperty(PropertyName = "Entrance", Required = Required.Default)]
     public EntranceViewModel Entrance { get; set; }
   }
-  public class QuoteViewModel {
-    public string Text { get;  set; }
+  public class QuoteViewModel
+  {
+    public string Text { get; set; }
   }
-  public class EntranceViewModel {
+  public class EntranceViewModel
+  {
+    [JsonProperty(PropertyName = "MovieMinute", Required = Required.Default)]
     public int MovieMinute { get; set; }
+    [JsonProperty(PropertyName = "SceneName", Required = Required.Default)]
     public string SceneName { get; set; }
+    [JsonProperty(PropertyName = "Description", Required = Required.Default)]
     public string Description { get; set; }
   }
 }
